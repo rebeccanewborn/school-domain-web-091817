4fe80069d33ebfb90cf6a02eeadafc0fef1acffd
rebeccanewborn <rjn58@cornell.edu>
2017-09-21 11:25:51 -0400

    finished

--- lib/school.rb
+++ lib/school.rb
@@ -1,1 +1,29 @@
 # code here!
+require 'pry'
+
+class School
+  attr_accessor :roster
+  attr_reader :name
+
+  def initialize(name)
+    @name = name
+    @roster = {}
+  end
+
+  def add_student(name, grade)
+    if roster[grade] == nil
+      roster[grade] = []
+    end
+    roster[grade] << name
+  end
+
+  def grade(num)
+    roster[num]
+  end
+
+  def sort
+    roster.each do |grade, array|
+      array.sort!
+    end
+  end
+end
